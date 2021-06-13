845314820e20a26c508e805ce0e12e6e3faf0746
Chris Mower <cmower@users.noreply.github.com>
2021-06-13 18:35:16 +0100

    Update description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pygame_teleop',
     version='0.1',
-    description='Simple pygame teleop.',
+    description='Tools for simple teleoperation examples/demos/experiments in PyGame.',
     url='https://github.com/cmower/pygame_teleop',
     author='Christopher E. Mower',
     author_email='chris.mower@ed.ac.uk',

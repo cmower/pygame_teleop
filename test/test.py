b0fe54b5a3f3512afc04664d166f60ec61fd55b1
Christopher Mower <chris.mower@ed.ac.uk>
2021-06-13 22:20:24 +0100

    Use keyboard when no joystick available

--- test/test.py
+++ test/test.py
@@ -1,14 +1,15 @@
 import sys
 import numpy
 import traceback
+import pygame
 from pygame_teleop.window import Window
-from pygame_teleop.human_input import HumanInputMode1
+from pygame_teleop.human_input import HumanInputMode1 as HumanInputJoystick
+from pygame_teleop.keyboard import HumanInputMode1 as HumanInputKeyboard
 
 
 def main():
 
     #### USER INPUT
-    joy_d0, joy_d1 = 1, 1
     robot_radius = 0.05
     hz = 30
     robot = numpy.array([0.5, 0.5])
@@ -17,7 +18,14 @@ def main():
     #### USER INPUT
 
     # Setup
-    human = HumanInputMode1(joy_d0, joy_d1)
+    try:
+        d0, d1 = 1, 1
+        use_keyb = False
+        human = HumanInputJoystick(d0, d1)
+    except pygame.error:
+        d0, d1 = -1, -1
+        use_keyb = True
+        human = HumanInputKeyboard(d0, d1, button_keys=[pygame.K_SPACE])
     dt = 1.0/float(hz)
     running = True
     window = Window(window_width)
@@ -28,8 +36,18 @@ def main():
         
         while running:
 
+            events = window.events()
+
+            if not use_keyb:
+                b = human.button(0)
+                h = human.get()
+            else:
+                human.reset(events)
+                b = human.button()[pygame.K_SPACE]
+                h = human.get()
+
             # Update robot
-            robot += max_vel*dt*human.get()
+            robot += max_vel*dt*h
             robot = numpy.clip(robot, 0, 1)
 
             # Update window
@@ -38,7 +56,7 @@ def main():
             window.update(hz)
 
             # Quit?
-            user_quit = window.did_user_quit(window.events()) or human.button(0)
+            user_quit = window.did_user_quit(events) or b
             running = not user_quit
             
     except KeyboardInterrupt:

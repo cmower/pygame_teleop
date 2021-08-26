8fd7d04501f136755de5180e07d1605bf4015cfd
Chris E. Mower <chris.mower@ed.ac.uk>
2021-08-26 10:08:31 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pygame_teleop',
-    version='1.0',
+    version='1.1',
     description='Tools for simple teleoperation examples/demos/experiments in PyGame.',
     url='https://github.com/cmower/pygame_teleop',
     author='Christopher E. Mower',
